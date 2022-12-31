907885ab09ca51dcfc5d53a18ebdfe77fd99151b
Maher E. Elmashharawi <mashmaher96@gmail.com>
2022-12-31 13:23:53 +0200

    edit the path for home page

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
