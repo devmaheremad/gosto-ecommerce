ff7a4ee773f3e591ca2a35c96770d654483867f4
Maher E. Elmashharawi <mashmaher96@gmail.com>
2022-12-31 15:32:14 +0200

    edit the home path

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/gosto-ecommerce" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
