3846fab0eb3ee3bf4577a7e3ec37e1ba59afd12b
Maher E. Elmashharawi <mashmaher96@gmail.com>
2022-12-31 13:34:14 +0200

    edit the path for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/gosto-ecommerce" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
