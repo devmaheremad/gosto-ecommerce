2fc1c99901888892fd49c991af3485080c521d4d
Maher E. Elmashharawi <mashmaher96@gmail.com>
2023-01-02 13:13:46 +0200

    put heading for testi

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,9 +1,15 @@
 import { Box } from "@mui/material";
+import { HeadingSection } from "./index";
 
 const Testimonials = () => {
   return (
     <Box mb={6}>
-      <h1>test</h1>
+      <HeadingSection
+        textAlign="center"
+        h4Words="Testimonials"
+        subtitle1Words="Looking for some reassurance before making a purchase? Check out testimonials for genuine customer feedback."
+        h4WordsColor="black"
+      />
     </Box>
   );
 };
